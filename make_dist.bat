ac6d727a615f759aff9bfd96f7dad54b0d7e9b93
tripod31 <tripod31@hotmail.com>
2015-06-08 16:45:15 +0900

    make_dist

--- make_dist.bat
+++ make_dist.bat
@@ -1,4 +1,4 @@
-set INC_DIR1="C:/devel_open/netbeans/delete_file/lib"
-set INC_DIR2="C:/devel_open/netbeans/common"
+set INC_DIR1="d:/devel_open/eclipse/tools_ruby/delete_file"
+set INC_DIR2="d:/devel_open/eclipse/tools_ruby/common"
 ruby -I %INC_DIR1% -I %INC_DIR2% make_dist.rb
 pause
--- make_dist.rb
+++ make_dist.rb
@@ -1,7 +1,7 @@
-require "FileUtils"
+# encoding: utf-8
 
+require "FileUtils"
 require "common"
-require "delete_file"
 
 APP_NAME="searchjar"
 COPY_FILES=
@@ -10,41 +10,28 @@ COPY_FILES=
     "bin/release/common.dll",
     "bin/release/ICSharpCode.SharpZipLib.dll",
     "readme.txt",
-    "C:/devel_open/dotnet/Common/src_common.zip",
     "COPYING",
     ]
-DELETE_SRC_PAT=['\.exe$','\.dll$','\.pdb$','\.suo','obj','make_dist','Release','Debug','ftp\.bat']
-DIST_DIR="c:/data_priv/hp/myhp/files"
+DIST_DIR="d:/devel_open/dotnet/serchjar"
+TEMP_DIR="d:/work"
 
-TEMP_DIR="c:/temp"
+SRC_DIR=File.expand_path(File.dirname($0))
 WORK_DIR=TEMP_DIR+"/"+APP_NAME
 ZIP_FILE=APP_NAME+".zip"
 
-#srcファイルコピー
-FileUtils.rm_r WORK_DIR,{:force=>true}
+FileUtils.rm_rf WORK_DIR
 FileUtils.mkdir WORK_DIR
-FileUtils.cp_r "../#{APP_NAME}", "#{WORK_DIR}/src"
-df = DeleteFile.new
-df.delete_files("#{WORK_DIR}/src",DELETE_SRC_PAT , true)
-
 COPY_FILES.each { |f|
     FileUtils.cp f,WORK_DIR
 }
 
-FileUtils.cd WORK_DIR
-ZIPDir("src.zip","src")
-FileUtils.rm_r "src"
-
 FileUtils.cd TEMP_DIR
 FileUtils.rm ZIP_FILE,{:force=>true}
 
-
-
-#zip.exeでハングするためziprubyを使用
+#zip.exe縺ｧ繝上Φ繧ｰ縺吶ｋ縺溘ａzipruby繧剃ｽｿ逕ｨ
 #puts `#{ZIP} -r #{ZIP_FILE} #{APP_NAME}/*`
 ZIPDir(ZIP_FILE,APP_NAME)
+FileUtils.mv ZIP_FILE, SRC_DIR
 
-#FileUtils.mv ZIP_FILE, DIST_DIR
-
-puts "#{TEMP_DIR}/#{ZIP_FILE}を作成しました。なにかキーを押してください"
+puts "#{ZIP_FILE}繧剃ｽ懈�舌＠縺ｾ縺励◆縲ゅ↑縺ｫ縺九く繝ｼ繧呈款縺励※縺上□縺輔＞"
 #gets
